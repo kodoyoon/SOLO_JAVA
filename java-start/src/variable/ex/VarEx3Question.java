deebf1b34baccfc73a69748ac9907c41967c4ff3
doyoon <doyoun0527@naver.com>
2023-12-12 16:53:48 +0900

    Hello World 그리고 변수

---
+++ java-start/src/variable/ex/VarEx3Question.java
@@ -0,0 +1,13 @@
+package variable.ex;
+
+public class VarEx3Question {
+  public static void main(String[] args) {
+    long longVar = 100000000L;
+    System.out.println("longVar = " + longVar);
+
+
+    boolean booleanVar = true;
+    System.out.println(booleanVar);
+        
+  }
+}
